978c7859e854d244ddf397b202d3d2f32db0a806
Ana Ortez <anaortez23@gmail.com>
2020-11-13 16:09:39 -0500

    Class Notes Corrected and Working

---
+++ Labs/manipulation/script.js
@@ -0,0 +1,89 @@
+console.log('script.js connected');
+
+// Add, update, remove attributes
+
+const paragraphNode = document.querySelector('#paragraph');
+
+// console.log(paragraphNode.id);
+
+const parId = paragraphNode.getAttribute('id');
+console.log('--->', parId);
+
+paragraphNode.setAttribute('id', 'info-paragraph');
+
+paragraphNode.setAttribute('class', 'paragraphs');
+
+paragraphNode.removeAttribute('id');
+
+//Add, update, remove elements
+
+// add --> create an element
+const mainTitle = document.createElement('h1');
+
+mainTitle.innerHTML = 'This is the main title';
+
+const parent = document.querySelector('body');
+parent.appendChild(mainTitle);
+
+//Insert before
+
+const sibling = document.querySelector('p');
+parent.insertBefore(mainTitle, sibling);
+
+//update
+
+const link = document.getElementById('google-link');
+link.innerHTML = 'Go to Google!';
+link.setAttribute('href', 'https://www.google.com/');
+link.setAttribute('target', '_blank');
+
+//remove
+parent.removeChild(paragraphNode);
+
+// assign events to dom elements
+const addCommentButton = document.querySelector('#add-comment-button');
+
+addCommentButton.addEventListener('click', () => {
+    //console.log('clicked on the comment button')
+
+    const newParagraph = document.createElement('p');
+    newParagraph.innerHTML = 'this is some very funny coment ha ha ha';
+    parent.appendChild(newParagraph);
+});
+
+
+
+
+
+const newItemInput = document.getElementById('item-input');
+const addItemButton = document.getElementById('add-item-button');
+const deleteButtons = document.querySelectorAll('.delete-item-button');
+
+addItemButton.addEventListener('click', () => {
+const listParent = document.getElementById('item-list');
+
+const newListItem = document.createElement('li');
+console.log(newItemInput.value);
+newListItem.innerHTML = `${newItemInput.value} <button class="delete-item-button">🗑</button>`;
+
+listParent.appendChild(newListItem);
+
+console.log(listParent.children);
+const newDeleteButton = newListItem.querySelector('.delete-item-button');
+
+deleteListItem(newDeleteButton);
+
+newItemInput.value = '';
+});
+
+function deleteListItem(buttonUserClickedOn){
+buttonUserClickedOn.addEventListener('click', () => {
+const parentLiTag = buttonUserClickedOn.parentElement;
+parentLiTag.remove();
+});
+}
+deleteButtons.forEach(deleteListItem);
+
+
+
+
